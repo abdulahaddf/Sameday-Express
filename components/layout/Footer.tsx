a08b3f5bbfdcb78e6bfaeca1f429d2fbe516ed95
Abdul Ahad <89897808+abdulahaddf@users.noreply.github.com>
2026-04-17 14:39:10 +0600

    Update Footer.tsx

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -177,7 +177,14 @@ export default function Footer() {
             </div>
           </div>
           <p className="text-grey-text text-[10px] text-center mt-3">
-            Design & Developed by <Link href="https://www.totalofftec.com" target="_blank" rel="noopener noreferrer" className="text-[#fc7901] transition-colors">TOTALOFFTEC</Link>
+            Design & Developed by <Link
+              href="https://www.totalofftec.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-[#fc7901] transition-colors tracking-widest font-bold"
+            >
+              TOTAL<span className="text-[#818286]">OFFTEC</span>
+            </Link>
           </p>
         </div>
       </div>
